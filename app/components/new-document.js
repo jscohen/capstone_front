2af6b8961423eb2adb89617b750b128a0a1d2910
jscohen <jon.cohen08@gmail.com>
2017-05-23 16:22:51 -0400

    Added bootstrap theme and some styling

--- app/components/new-document.js
+++ app/components/new-document.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
-
-document.querySelector('textarea').addEventListener('mouseup', function () {
-  window.mySelection = this.value.substring(this.selectionStart, this.selectionEnd)
-  // window.getSelection().toString();
-})
-
+//
+// document.querySelector('textarea').addEventListener('mouseup', function () {
+//   window.mySelection = this.value.substring(this.selectionStart, this.selectionEnd)
+//   // window.getSelection().toString();
+// })
+//
 export default Ember.Component.extend({
   actions: {
     getInput() {
